a22da2d7af0c7b2888283ecb35a28fc499c4ea5c
kitrawee-th <atomatomatom987@gmail.com>
2025-07-14 17:23:23 +0700

    work2

---
+++ coding_04-2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>  // ต้องมี#และใช้<>   
+int main() {   //main function
+    int a = 10;  //ต้องมี ; ท้ายบรรทัด
+    printf("a = %d", a);  //แสดงค่าa
+    return 0;  //ต้องมี ; ท้ายบรรทัด
+}
