8c6ab867b66636113363a7f8d771b46743b74ad3
kitrawee-th <atomatomatom987@gmail.com>
2025-07-21 13:03:26 +0700

    wrok 3

--- coding_04-3.c
+++ coding_04-3.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    char nameInitial = 'K';            
-    int age = 20;                     
-    float weight = 48.50;              
-    char gender[] = "Female";          
+    // ประกาศตัวแปร
+    char nameInitial = 'K';            //ตัวอักษรแรกของชื่อ
+    int age = 20;                     //อายุ (จำนวนเต็ม)
+    float weight = 48.50;           //น้ำหนัก(จำนวนทศนิยม 1 ตำแหน่ง)   
+    char gender[] = "Female";          //เพศ (เป็นข้อความ)
 
     
     printf("nameInitial = %c\n", nameInitial);
